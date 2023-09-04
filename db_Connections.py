0eeeca3f487c6ef9712713084fa6ff0f432db4b3
vishnu-003 <svishnuvardhan1209@gmail.com>
2023-09-04 14:40:39 +0530

    add gmail.py for testing

--- db_Connections.py
+++ db_Connections.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 import pymysql
 
 app=Flask(__name__)
@@ -20,7 +21,22 @@ def db_connection():
 
     return connection
 
- 
+mail= Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'liarchary007gmail.com'
+app.config['MAIL_PASSWORD'] = 'pevf qmil csek nvzm'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+@app.route("/")
+def index(email):
+   msg = Message('Hello', sender = 'liarchary007@gmail.com', recipients = ['{email}'])
+   msg.body = "Hello Flask message sent from Flask-Mail"
+   mail.send(msg)
+   return "Sent"
 
 @app.route('/', methods =['GET', 'POST'])
 
@@ -33,7 +49,7 @@ def login():
         password=request.form["password"]
         conn = db_connection()
         cur = conn.cursor()
-        Query = f"SELECT email,passwrd FROM new1 WHERE email='{email}' AND passwrd='{password}';"
+        Query = f"SELECT email,passwrd FROM Users WHERE email='{email}' AND passwrd='{password}';"
         print(Query)
         cur.execute(Query)
         details=cur.fetchall()
@@ -60,8 +76,11 @@ def register():
         password=request.form["password"]
         conn = db_connection()
         cur = conn.cursor()
-        Query = f"INSERT INTO `defaultdb`.`new1` (`lastname`, `firstname`, `email`, `mobile`, `city`,`passwrd`) VALUE ('{lastname}', '{firstname}', '{email}', '{mobile}', '{city}','{password}');"
+        Query = f"INSERT INTO `defaultdb`.`Users` (`lastname`, `firstname`, `email`, `mobile`, `city`,`passwrd`) VALUE ('{lastname}', '{firstname}', '{email}', '{mobile}', '{city}','{password}');"
         print(Query)
+
+        index(email)
+        
         cur.execute(Query)
         conn.commit()
         cur.close()
@@ -69,6 +88,9 @@ def register():
         print(cur.fetchall())
 
         return render_template("login.html")
+    
+
+    
 
         
 
